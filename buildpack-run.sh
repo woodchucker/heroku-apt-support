eeff2c00da041f924f701abf2b7317fb8060acd8
woodchucker <ditrafabri@gmail.com>
2020-06-24 16:10:57 +0200

    0

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -2,4 +2,7 @@
 # tar -xvjf curl-7.46.0.tar.bz2  
 # cd curl-7.46.0  
 # ./configure  --disable-shared --with-nghttp2 --prefix=/app/.apt/usr
-# make 
+# make
+wget http://i122server.vu-wien.ac.at/MSA/Software/MSA_lin.tar.gz
+tar -zxvf MSA_lin.tar.gz
+cd MSA4.05
