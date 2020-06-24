8289c9d7bc5069aae7d46c1e722bdda3e9b5deb0
woodchucker <ditrafabri@gmail.com>
2020-06-24 16:03:48 +0200

    0

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,5 @@
-wget http://curl.haxx.se/download/curl-7.46.0.tar.bz2  
-tar -xvjf curl-7.46.0.tar.bz2  
-cd curl-7.46.0  
-./configure  --disable-shared --with-nghttp2 --prefix=/app/.apt/usr
-make 
+# wget http://curl.haxx.se/download/curl-7.46.0.tar.bz2  
+# tar -xvjf curl-7.46.0.tar.bz2  
+# cd curl-7.46.0  
+# ./configure  --disable-shared --with-nghttp2 --prefix=/app/.apt/usr
+# make 
